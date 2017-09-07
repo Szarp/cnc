2daf422524f95ddfe8867933f1177277d1338261
szarp <lizusfull@gmail.com>
2017-09-07 23:02:45 +0200

    automatic creation form panels with chcekboxes

--- public/js/sendForm.js
+++ public/js/sendForm.js
@@ -18,16 +18,16 @@ var btnEvents ={
     saveBtn:function(){takeValuesFromForm()},
     generateBtn:function(){tokenValidation('generateToken')},
     checkBtn:function(){tokenValidation('checkToken')},
-    upArr:function(){cncActions("move",[1,0,0])},
-    downArr:function(){cncActions("move",[-1,0,0])},
+    //upArr:function(){cncActions("move",[1,0,0],this)},
+    //downArr:function(){cncActions("move",[-1,0,0])},
     upArrWork:function(){cncActions("work",[1,0,0])},
-    downArrWork:function(){cncActions("work",[-1,0,0])},
-    leftArr:function(){cncActions("move",[0,1,0])},
-    rightArr:function(){cncActions("move",[0,-1,0])},
-    topArr:function(){cncActions("move",[0,0,1])},
+    downArrWork:function(){cncActions("work",[-1,0,0]);setTimeout(function(){cncActions("work",[1,0,0]);},1000)},
+    //leftArr:function(){cncActions("move",[0,1,0])},
+    //rightArr:function(){cncActions("move",[0,-1,0])},
+    //topArr:function(){cncActions("move",[0,0,1])},
     control:function(){movePannnels("controlPannel")},
     work:function(){movePannnels("workPannel")},
-    bottomArr:function(){cncActions("move",[0,0,-1])},
+    //bottomArr:function(){cncActions("move",[0,0,-1])},
     clearBtn:function(){cncActions("setZero")}
 }
 function movePannnels(name){
@@ -39,27 +39,208 @@ function movePannnels(name){
         if(pannelList[i] != name)
             el.style.display = "none"
         else
-            el.style.display = "block"
+            el.style.display = ""
     }
 }
-function closeMsg(elID){
-	var msgArea = document.getElementById('msgArea');
-	var toRemove = document.getElementById(elID);
-	msgArea.removeChild(toRemove);
+//setTimeout(function(){makeCheckbox("onePanel_checklist",[10,1,0.1],"checkbox");},2000);
+
+function clear(self){
+    
+    console.log(parent);
+    
 }
-
-function copy(elem, msg){
-	var succeed = copyToClipboard(elem);
-	if(!succeed){
-		alert("Copy not supported or blocked. Press Ctrl+c to copy.");
-		document.getElementById(msg).innerHTML = "Błąd kopiowania";
-	} else {
-		document.getElementById(msg).innerHTML = "Skopiowano";
-	}
+var panel = new Panel();
+setTimeout(function(){console.log(panel.addPanel("controlPannel",{
+        checkbox:[10,1,0.1],
+        checked:-1,
+        ulClass:"checkbox",
+        arrayDirection:["left","right"],
+        arrayEvents:[function(){cncActions("move",[1,0,0],this)},function(){cncActions("move",[-1,0,0],this)}],
+        id:"xPanel",
+        name:"X panel"
+    }));
+    panel.addPanel("controlPannel",{
+        checkbox:[10,1,0.1],
+        checked:-1,
+        ulClass:"checkbox",
+        arrayDirection:["up","down"],
+        arrayEvents:[function(){cncActions("move",[0,1,0],this)},function(){cncActions("move",[0,-1,0],this)}],
+        id:"yPanel",
+        name:"Y panel"
+    });
+    panel.addPanel("controlPannel",{
+        checkbox:[10,1,0.1],
+        checked:-1,
+        ulClass:"checkbox",
+        arrayDirection:["up","down"],
+        arrayEvents:[function(){cncActions("move",[0,0,1],this)},function(){cncActions("move",[0,0,-1],this)}],
+        id:"zPanel",
+        name:"Z panel"
+    });
+                     },1000);
+function Panel(){
+    var self=this;
+    this.panelList=[];
+    /*var panelDataEx={
+        checkbox:[10,1,0.1],
+        checked:-1,
+        ulClass:"checkbox",
+        arrayDirection:["up","down"],
+        id:"onePanel"
+    }*/
+    this.addPanel=function(dest,json){
+        //var newPanel = self.createPanel(json)
+        //self.createTable(dest,json);
+        var newPanel=self.prepareTable(json);
+        //console.log(document.getElementById(dest));
+        document.getElementById(dest).innerHTML+=newPanel;
+        self.panelList.push(json);
+        self.makeCheckbox(json);
+        self.updateEventsToCheckbox(dest);
+        self.updateEventsToArrays(dest);
+        
+    }
+    this.prepareTable=function(json){
+        return '<table id="'+json["id"]+'" class="arrays"><tr><td colspan="2">'+json["name"]+'</td></tr><tr><td colspan="2" id="'+json["id"]+'_checkbox"></td></tr><tr>'+self.prepareArrays(json["id"],json["arrayDirection"])+'</tr></table>';
+    }
+    this.prepareArrays=function(id,arrays){
+        eventsAttach.call(this);
+        var elems=""
+        var marks={
+            up:"&uarr;",
+            down:"&darr;",
+            left:"&larr;",
+            right:"&rarr;"
+        }
+        for(var i=0;i<2;i++){
+            elems+='<td id="'+id+'_'+arrays[i]+'">'+marks[arrays[i]]+'</td>';
+        }
+        return elems;
+    }
+    this.checkValues = function(self){
+        var list = self.parentElement.parentElement;
+        var input= list.getElementsByTagName('input');
+        //console.log("s",input);
+        var i;
+        var index=null;
+        for(i=0;i<input.length;i++){
+            if(input[i].checked == true){
+                return input[i].value;
+                //break;
+            }
+        }
+        return 0;
+    }
+    this.makeCheckbox = function(json){
+        var el='<ul class="'+json["ulClass"]+'">';
+        var params = json["checkbox"];
+        for(var i=0;i<params.length;i++){
+            el += "<li>";
+            el +='<input type="checkbox"  name="multiplier" value="'+params[i]+'"><label>'+params[i]+'</label>';
+            el+= "</li>";
+            //base.appendChild(el);
+        }
+        el+="</ul>";
+        var  dest = json["id"]+"_checkbox";
+        document.getElementById(dest).innerHTML=el;
+    }
+    this.updateEventsToArrays=function(dest){
+        /*
+        var el = document.getElementById(dest);
+        var x = dest.nod
+        var clone = el.cloneNode(true);
+        el.parentNode.replaceChild(clone, clone);
+        */
+        //console.log(clone);
+        var el,
+            panel,
+            event;
+        //console.log("hi");
+        console.log(self.panelList[0]["arrayDirection"]);
+        for(var i=0;i<self.panelList.length;i++){
+            panel=self.panelList[i]
+            for(var j=0;j<panel["arrayDirection"].length;j++){
+                el = document.getElementById(panel["id"]+"_"+panel["arrayDirection"][j])
+                event=panel["arrayEvents"][j];
+                el.removeEventListener('click',event,false);
+                //console.log(el,event)
+                el.addEventListener('click',event,false);
+            }
+            
+            
+        }
+    }
+    this.updateEventsToCheckbox=function(dest){
+        var el = document.getElementById(dest);
+        var elem=el.getElementsByTagName('input');
+        //console.log(elem)
+        for(var i=0;i<elem.length;i++){
+            elem[i].addEventListener('click',function(){
+                var parent = this.parentElement.parentElement;
+                var elems = parent.getElementsByTagName('input');
+                for(var i=0;i<elems.length;i++){
+                    if(elems[i] != this)
+                        elems[i].checked=false;
+                }
+                console.log(this.value);
+            },false);
+        }    
+    }
+    //panel list
+    //checkbox values
+    //array direction
+    //panel id
+    //createPanel
 }
-
-
-
+function cncActions(type,value,self){
+    //console.log('hi');
+    var form={};
+    //form['speed'] = document.getElementById('speed').value;
+    //form['notification'] = document.getElementById('setNotification').value;
+    var url = 'postCall';
+    form['mode'] = 'cnc';
+    form['type'] = type;
+    if(type == "move"){
+        //console.log("input test",panel.checkValues(self));
+        var factor = panel.checkValues(self)
+        console.log('text',Number(factor));
+        if(Number(factor) != NaN){
+            value[0]*=factor;
+            value[1]*=factor;
+            value[2]*=factor;
+        }
+    }
+    else if (type=="point"){
+        
+    }
+    else if (type=="setZero"){
+        
+    }
+    else if (type=="work"){
+        var el = document.getElementById("stepWork").getElementsByTagName("textArea")[0].value;
+        console.log('text',Number(el));
+        if(Number(el) != NaN){
+            value[0]*=el;
+            value[1]*=el;
+            value[2]*=el;
+        
+    }
+    }
+    form['value'] = value;
+    //console.log(form);
+    //z.setClassName(form.setClass);
+    //z.displayData();
+    //console.log("send Form:",form);
+    sendObj(url,form,function(obj){
+        //var json = JSON.parse(obj); 
+        //updateCoorinates(obj["xyzVal"])
+        console.log("obj",obj);
+        if(obj["type"]=='pos'){
+            updateCoorinates(obj);
+        }
+        console.log('cnc control',obj,form);
+    });
+}
 function tokenValidation(mode){
     var url = 'postCall';
     var form = {};
@@ -127,54 +308,6 @@ function takeValuesFromForm(){
         console.log('saveSettings',obj);
     });
     //console.log('param from form',a,b);
-}
-function cncActions(type,value){
-    //console.log('hi');
-    var form={};
-    //form['speed'] = document.getElementById('speed').value;
-    //form['notification'] = document.getElementById('setNotification').value;
-    var url = 'postCall';
-    form['mode'] = 'cnc';
-    form['type'] = type;
-    if(type == "move"){
-        var el = document.getElementById("step").getElementsByTagName("textArea")[0].value;
-        console.log('text',Number(el));
-        if(Number(el) != NaN){
-            value[0]*=el;
-            value[1]*=el;
-            value[2]*=el;
-        }
-    }
-    else if (type=="point"){
-        
-    }
-    else if (type=="setZero"){
-        
-    }
-    else if (type=="work"){
-        var el = document.getElementById("stepWork").getElementsByTagName("textArea")[0].value;
-        console.log('text',Number(el));
-        if(Number(el) != NaN){
-            value[0]*=el;
-            value[1]*=el;
-            value[2]*=el;
-        
-    }
-    }
-    form['value'] = value;
-    //console.log(form);
-    //z.setClassName(form.setClass);
-    //z.displayData();
-    //console.log("send Form:",form);
-    sendObj(url,form,function(obj){
-        //var json = JSON.parse(obj); 
-        //updateCoorinates(obj["xyzVal"])
-        console.log("obj",obj);
-        if(obj["type"]=='pos'){
-            updateCoorinates(obj);
-        }
-        console.log('cnc control',obj);
-    });
 }
 function floor(num){
     return Math.floor(num*100)/100;
@@ -338,8 +471,21 @@ function homePosition(id){
             }
         }
 }
+function eventsAttach(){
+    var self = this;
+    this.btnEvents={};
+    this.addClicks=function(){
+        for(k in self.btnEvents){
+            var el = document.getElementById(k);
+            //console.log(el);
+            el.addEventListener('click',this.btnEvents[k],false);
+            //console.log(this.btnEvents[k]);
+        }
+    }
+}
 function traslateSettings(){
     this.formId = 'setClass';
+    eventsAttach.call(this);
     this.saveData = function(data){
         this.changeDisplayEvents = data['changeDisplayEvents'];
         this.btnEvents=btnEvents;
@@ -359,13 +505,19 @@ function traslateSettings(){
         el.addEventListener('click',function(){ homePosition(key[1])},false);
         }
     }
-    this.addClicks=function(){
-        for(k in this.btnEvents){
-            var el = document.getElementById(k);
-            //console.log(el);
-            el.addEventListener('click',this.btnEvents[k],false);
-            //console.log(this.btnEvents[k]);
-        }
-    }
+}
+function closeMsg(elID){
+	var msgArea = document.getElementById('msgArea');
+	var toRemove = document.getElementById(elID);
+	msgArea.removeChild(toRemove);
 }
 
+function copy(elem, msg){
+	var succeed = copyToClipboard(elem);
+	if(!succeed){
+		alert("Copy not supported or blocked. Press Ctrl+c to copy.");
+		document.getElementById(msg).innerHTML = "Błąd kopiowania";
+	} else {
+		document.getElementById(msg).innerHTML = "Skopiowano";
+	}
+}
